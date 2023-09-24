877f11eeba5f1c6a60ccaab01c08b8b628fbb198
Gelado047 <juniorthesouza017@gmail.com>
2023-09-24 15:58:33 -0300

    hot fix

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.get('/hello-world', async (req, res) => {
+app.get('/hello-world', (req, res) => {
     const name = req.query.name || `world`;
     res.send(`Hello ${name}!`);
 });
 
-app.get('/hello-world/:name', async(req, res) => {
+app.get('/hello-world/:name', (req, res) => {
     const name = req.query.name;
     res.send(`Hello ${name}`);
 });

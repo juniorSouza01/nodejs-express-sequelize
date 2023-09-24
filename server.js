f14dea2eb3ae483e726b726b96fb85c66cab8135
Gelado047 <juniorthesouza017@gmail.com>
2023-09-24 15:44:19 -0300

    server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = require('./routes/apiRotes');
+const PORT = process.env.PORT || 3000;
+
+const startServer = async () => {
+    try{
+        app.listen(PORT, () => {
+            console.log(`listening on: http://localhost:${PORT}`);
+        });
+    } catch(error) {
+        console.error('Error starting server: ', error);
+    }
+};
